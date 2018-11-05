cf19216216ee9188600aa385c273b887d8ddf342
Mike Rovers <mikerovers97@gmail.com>
2018-11-05 15:47:34 +0100

    Added SFML

--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+#include "src/Vector.h"
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Lineaire dingen");
+
+    std::vector<Vector*> vectors;
+    vectors.push_back(new Vector(60, 60));
+    vectors.push_back(new Vector(120, 400));
+    vectors.push_back(new Vector(50, 30));
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while(window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color::Black);
+
+        sf::RectangleShape yLine(sf::Vector2f(600.f, 1.f));
+        yLine.setPosition(400, 0);
+        yLine.rotate(90);
+        window.draw(yLine);
+
+        sf::RectangleShape xLine(sf::Vector2f(1200.f, 1.f));
+        xLine.setPosition(0, 300);
+        window.draw(xLine);
+
+        window.display();
+    }
+
     return 0;
 }
