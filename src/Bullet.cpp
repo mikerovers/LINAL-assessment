d17989009ca6c08b61df5fd026e4b158ca439c21
WouterBouwman <wouter.bouwman@hotmail.com>
2019-01-15 17:52:32 +0100

    Added some movement changes

--- src/Bullet.cpp
+++ src/Bullet.cpp
@@ -8,26 +8,38 @@ Bullet::Bullet(const Matrix &other) : Matrix(other)
 
 void Bullet::act()
 {
-    translate(direction.getX(), direction.getY(), direction.getZ());
-    setLifetime(getLifetime() - 1);
+	double x, y, z;
+	if (hasSpeedBoost) {
+		x = direction.getX() + speedVector.getX();
+		y = direction.getY() + speedVector.getY();
+		z = direction.getZ() + speedVector.getZ();
+	}
+	else {
+		x = direction.getX();
+		y = direction.getY();
+		z = direction.getZ();
+	}
+
+	translate(x, y, z);
+	setLifetime(getLifetime() - 1);
 }
 
 const Vector3D &Bullet::getDirection() const
 {
-    return direction;
+	return direction;
 }
 
 void Bullet::setDirection(const Vector3D &direction)
 {
-    Bullet::direction = direction;
+	Bullet::direction = direction;
 }
 
 int Bullet::getLifetime() const
 {
-    return lifetime;
+	return lifetime;
 }
 
 void Bullet::setLifetime(int lifetime)
 {
-    Bullet::lifetime = lifetime;
+	Bullet::lifetime = lifetime;
 }
--- src/Player.h
+++ src/Player.h
@@ -10,7 +10,24 @@ public:
 	void act() override;
 	int getShootTimer() const;
 	void setShootTimer(int shootTimer);
-private:
-	int shootTimer;
-};
+	Vector3D getSpeedVector() const { return speedVector; }
 
+	int getSpeed() const { return speed; }
+	void increaseSpeed() {
+		if (speed < MAXSPEED) { speed++; }
+		else { speed = MAXSPEED; }
+	}
+	void decreaseSpeed() {
+		if (speed > -MAXSPEED) { speed--; }
+		else { speed = -MAXSPEED; }
+	}
+
+	void setHeading(Vector3D newHeading) { heading = newHeading; }
+
+private:
+	int shootTimer = 0;
+	Vector3D heading;
+	Vector3D speedVector;
+	int speed = 0;
+	int const MAXSPEED = 15;
+};
